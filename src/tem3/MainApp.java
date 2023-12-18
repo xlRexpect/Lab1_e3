a55d51082bc77766f021c94f3203c3759d46a1ac
xlRexpect <dragosflorea02@yahoo.com>
2023-12-18 19:59:42 +0200

    HUSDSDF

--- src/tem3/MainApp.java
+++ src/tem3/MainApp.java
@@ -1,2 +1,23 @@
-package tem3;public class MainApp {
+package tem3;
+
+import java.util.Scanner;
+
+public class MainApp {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("n=");
+        int n=scanner.nextInt();
+        int i=2;
+        int a=1;
+        while(i<n){
+            if(n%i==0){
+                System.out.println(i);
+                a=0;
+            }
+            i++;
+        }
+        if(a==1){
+            System.out.println("numarul este prim");
+        }
+    }
 }
